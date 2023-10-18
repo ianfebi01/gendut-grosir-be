09c79d7cfe91b64ca0da345fd29dce41cc15c9a6
ianfebi01 <ianfebi01@gmail.com>
2023-10-18 16:20:59 +0700

    fix error add new category and change response status

--- controllers/category.js
+++ controllers/category.js
@@ -1,41 +1,46 @@
-const Category = require("../models/Category");
+const Category = require('../models/Category')
+const status = require('http-status')
 
 exports.postCategory = async (req, res) => {
   try {
-    const { name } = req.body;
+    const { name } = req.body
     if (name) {
       // : { $regex: new RegExp("^" + name.toLowerCase(), "i") }
       const duplicate = await Category.findOne({
         name,
-      });
+      })
       if (duplicate) {
-        return res.status(400).json({
-          message: "Category name already exists",
-        });
+        return res.status(status.BAD_REQUEST).json({
+          message: 'Category name already exists',
+        })
       }
       const category = await new Category({
         name,
-      }).save();
+      }).save()
 
-      res.json({
-        message: "Successfully created new category",
+      return res.status(status.CREATED).json({
+        message: 'Successfully created new category',
         data: category,
-      });
+      })
     }
-    res.status(500).json({ message: "Name can't blank" });
+    return res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: "Name can't blank" })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
   }
-};
+}
 
 exports.getCategory = async (req, res) => {
   try {
-    const { q, limit, page } = req.query;
+    const { q, limit, page } = req.query
     const myCustomLabels = {
-      totalDocs: "itemCount",
-      docs: "data",
-      meta: "paginator",
-    };
+      totalDocs: 'itemCount',
+      docs: 'data',
+      meta: 'paginator',
+    }
 
     const options = {
       limit: limit || 25,
@@ -44,41 +49,48 @@ exports.getCategory = async (req, res) => {
         createdAt: -1,
       },
       customLabels: myCustomLabels,
-    };
+    }
     const agregate = Category.aggregate([
-      { $match: { name: { $regex: q || "", $options: "i" } } },
+      { $match: { name: { $regex: q || '', $options: 'i' } } },
       {
         $lookup: {
-          from: "products",
-          localField: "_id",
-          foreignField: "category",
-          as: "products",
+          from: 'products',
+          localField: '_id',
+          foreignField: 'category',
+          as: 'products',
         },
       },
       {
         $project: {
           _id: 1,
           category: 1,
-          totalProducts: { $size: "$products" },
-          name: "$name",
+          totalProducts: { $size: '$products' },
+          name: '$name',
         },
       },
-    ]);
-    const category = await Category.aggregatePaginate(agregate, options);
-    res.json(category);
+    ])
+    const category = await Category.aggregatePaginate(agregate, options)
+    return res.status(status.OK).json(category)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
   }
-};
+}
 
 exports.deleteCategory = async (req, res) => {
   try {
-    const category = await Category.findOneAndDelete({ _id: req.params.id });
-    res.json({ message: "Successfully deleted category", data: category });
+    const category = await Category.findOneAndDelete({ _id: req.params.id })
+    return res.json({
+      message: 'Successfully deleted category',
+      data: category,
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
   }
-};
+}
 
 exports.updateCategory = async (req, res) => {
   try {
@@ -88,16 +100,18 @@ exports.updateCategory = async (req, res) => {
       {
         new: true,
       }
-    );
+    )
     if (category) {
       return res.json({
-        message: "Successfully edited category",
+        message: 'Successfully edited category',
         data: category,
-      });
+      })
     } else {
-      return res.status(404).json({ message: "Data not found" });
+      return res.status(status.NOT_FOUND).json({ message: 'Data not found' })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message })
   }
-};
+}
