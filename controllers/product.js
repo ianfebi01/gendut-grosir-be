17bae7d00297dd6cd92907a3cf5aa08aad1ab5be
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2023-12-21 18:13:15 +0700

    change flow add/edit product

--- controllers/product.js
+++ controllers/product.js
@@ -1,138 +1,152 @@
-const { find } = require("../models/Product");
-const Product = require("../models/Product");
+const { find } = require('../models/Product')
+const Product = require('../models/Product')
+const { uploadToCloudinary, uploadSingleImage } = require('./upload')
 
 exports.postProduct = async (req, res) => {
   try {
-    const { name } = req.body;
-    const payload = req.body;
-    // name: { $regex: new RegExp("^" + name.toLowerCase(), "i") },
+    const { name } = req.body
+    const payload = req.body
+
     const duplicate = await Product.findOne({
       name,
-    });
+    })
     if (duplicate) {
       return res.status(400).json({
-        message: "Product name already exists",
-      });
+        message: 'Product name already exists',
+      })
     }
     if (!payload.stock) {
-      payload.stock = 0;
+      payload.stock = 0
+    }
+    const uploadImage = await uploadSingleImage(req, res)
+
+    if (uploadImage) {
+      payload.image = uploadImage
     }
 
-    const product = await new Product({ ...req.body }).save();
+    const product = await new Product({ ...payload }).save()
 
     const productPopulate = await Product.findOne({
       _id: product._id,
-    }).populate("category", "name");
+    }).populate('category', 'name')
     res.json({
-      message: "Successfully post data",
+      message: 'Successfully post data',
       data: productPopulate,
-    });
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.getProduct = async (req, res) => {
   try {
-    const { q, limit, page, category } = req.query;
+    const { q, limit, page, category } = req.query
     const myCustomLabels = {
-      totalDocs: "itemCount",
-      docs: "data",
-      meta: "paginator",
-    };
+      totalDocs: 'itemCount',
+      docs: 'data',
+      meta: 'paginator',
+    }
 
     const options = {
       limit: limit || 25,
       page: page || 1,
-      populate: { path: "category", select: "name" },
+      populate: { path: 'category', select: 'name' },
       sort: {
         createdAt: 1,
       },
       customLabels: myCustomLabels,
-    };
-    let product = null;
+    }
+    let product = null
     if (category) {
       // filter by category
       product = await Product.paginate(
         {
-          name: { $regex: q || "", $options: "i" },
+          name: { $regex: q || '', $options: 'i' },
           category,
         },
         options
-      );
+      )
     } else {
       // no filter category
       product = await Product.paginate(
         {
-          name: { $regex: q || "", $options: "i" },
+          name: { $regex: q || '', $options: 'i' },
         },
         options
-      );
+      )
     }
 
     res.json({
-      message: "Successfully get data",
+      message: 'Successfully get data',
       data: product,
-    });
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.getProductById = async (req, res) => {
   try {
     const product = await Product.findOne({ _id: req.params.id }).populate(
-      "category",
-      "name"
-    );
+      'category',
+      'name'
+    )
     if (!product) {
-      return res.status(404).json({ message: "Product not found" });
+      return res.status(404).json({ message: 'Product not found' })
     }
     res.json({
-      message: "Successfully get data",
+      message: 'Successfully get data',
       data: product,
-    });
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 exports.getProductByBarcode = async (req, res) => {
   try {
     const product = await Product.findOne({
       barcode: req.params.barcode,
-    }).populate("category", "name");
+    }).populate('category', 'name')
     if (!product) {
-      return res.status(404).json({ message: "Produk tidak ditemukan" });
+      return res.status(404).json({ message: 'Produk tidak ditemukan' })
     }
 
     res.json({
-      message: "Successfully get data",
+      message: 'Successfully get data',
       data: product,
-    });
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.updateProduct = async (req, res) => {
   try {
+    const payload = req.body
+
+    const uploadImage = await uploadSingleImage(req, res)
+
+    if (uploadImage) {
+      payload.image = uploadImage
+    }
+
     const product = await Product.findOneAndUpdate(
       { _id: req.params.id },
-      { ...req.body },
+      { ...payload },
       { new: true }
-    ).populate("category", "name");
+    ).populate('category', 'name')
     if (product) {
       return res.json({
-        message: "Successfully updated product",
+        message: 'Successfully updated product',
         data: product,
-      });
+      })
     } else {
-      return res.status(404).json({ message: "Data not found" });
+      return res.status(404).json({ message: 'Data not found' })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.updateProductStockByBarcode = async (req, res) => {
   try {
@@ -144,32 +158,32 @@ exports.updateProductStockByBarcode = async (req, res) => {
         },
       },
       { new: true }
-    ).populate("category", "name");
+    ).populate('category', 'name')
     if (product) {
       return res.json({
-        message: "Successfully updated stock",
+        message: 'Successfully updated stock',
         data: product,
-      });
+      })
     } else {
-      return res.status(404).json({ message: "Produk tidak ditemukan" });
+      return res.status(404).json({ message: 'Produk tidak ditemukan' })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 exports.deleteProduct = async (req, res) => {
   try {
-    const product = await Product.findByIdAndRemove(req.params.id);
+    const product = await Product.findByIdAndRemove(req.params.id)
     if (product) {
       return res.json({
-        message: "Successfully delete data",
+        message: 'Successfully delete data',
         data: product,
-      });
+      })
     } else {
-      return res.status(404).json({ message: "Data not found" });
+      return res.status(404).json({ message: 'Data not found' })
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
--- controllers/upload.js
+++ controllers/upload.js
@@ -1,48 +1,49 @@
-const cloudinary = require("cloudinary");
-const fs = require("fs");
+const cloudinary = require('cloudinary')
+const fs = require('fs')
+const imageUpload = require('../middlewares/imageUpload')
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-});
+})
 
 exports.uploadImages = async (req, res) => {
   try {
-    const { path } = req.body;
-    let files = Object.values(req.files).flat();
-    let images = [];
+    const { path } = req.body
+    let files = Object.values(req.files).flat()
+    let images = []
     for (const file of files) {
-      const url = await uploadToCloudinary(file, path);
-      images.push(url);
-      removeTmp(file.tempFilePath);
+      const url = await uploadToCloudinary(file, path)
+      images.push(url)
+      removeTmp(file.tempFilePath)
     }
-    res.json(images);
+    res.json(images)
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message })
   }
-};
+}
 exports.deleteImage = async (req, res) => {
   try {
-    const { publicId } = req.body;
+    const { publicId } = req.body
     const delImg = await cloudinary.v2.uploader.destroy(publicId, {
-      resource_type: "image",
-    });
+      resource_type: 'image',
+    })
     //   .then((result) => console.log(result));
-    res.json(delImg);
+    res.json(delImg)
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message })
   }
-};
+}
 
 const uploadToCloudinary = async (file, path) => {
-  return await cloudinary.v2.uploader.upload(
+  const res = await cloudinary.v2.uploader.upload(
     file.tempFilePath,
-    { folder: path, tags: "basic_sample" },
+    { folder: path, tags: 'basic_sample' },
     function (err, image) {
       // console.log(image);
       // console.log("** File Upload");
       if (err) {
-        console.warn(err);
+        console.warn(err)
         // return res.status(400).json({ message: "Upload image failed." });
       }
       // console.log(
@@ -52,7 +53,10 @@ const uploadToCloudinary = async (file, path) => {
       // console.log("* " + image.url);
       // waitForAllUploads(file.tempFilePath, err, image);
     }
-  );
+  )
+  removeTmp(file.tempFilePath)
+
+  return res
   // return new Promise((resolve) => {
   //   cloudinary.v2.uploader.upload(
   //     file.tempFilePath,
@@ -71,10 +75,58 @@ const uploadToCloudinary = async (file, path) => {
   //     }
   //   );
   // });
-};
+}
+
+exports.uploadSingleImage = async (req, res) => {
+  let files = []
+  await validateImage(req, res)
+
+  if (req.files) {
+    files = Object.values(req?.files).flat()
+  }
+
+  let imageUrl = ''
+
+  if (files?.length === 1) {
+    const imageData = await uploadToCloudinary(files[0], 'gendut-grosir')
+    imageUrl = imageData?.secure_url
+  }
+
+  if (imageUrl !== '') {
+    return imageUrl
+  } else return false
+}
 
 const removeTmp = (path) => {
   fs.unlink(path, (err) => {
-    if (err) throw err;
-  });
-};
+    if (err) throw err
+  })
+}
+
+exports.uploadToCloudinary = uploadToCloudinary
+
+const validateImage = async (req, res) => {
+  try {
+    if (!req.files || Object.values(req.files).flat().length === 0) {
+      return res.status(400).json({ message: 'No files selected.' })
+    }
+    let files = Object.values(req.files).flat()
+    files.forEach((file) => {
+      if (
+        file.mimetype !== 'image/jpeg' &&
+        file.mimetype !== 'image/png' &&
+        file.mimetype !== 'image/gif' &&
+        file.mimetype !== 'image/webp'
+      ) {
+        removeTmp(file.tempFilePath)
+        return res.status(400).json({ message: 'Unsupported format.' })
+      }
+      if (file.size > 1024 * 1024 * 10) {
+        removeTmp(file.tempFilePath)
+        return res.status(400).json({ message: 'File size is too large.' })
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
